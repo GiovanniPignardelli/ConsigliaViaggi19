c9445d902599ba14f502902a3a41975436d20134
Giovanni <g.pignardelli@studenti.unina.it>
2020-09-02 17:35:25 +0200

    Inizializzata mainpage

--- app/src/main/java/it/gpgames/consigliaviaggi19/MainActivity.java
+++ app/src/main/java/it/gpgames/consigliaviaggi19/MainActivity.java
@@ -1,32 +1,45 @@
 package it.gpgames.consigliaviaggi19;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    Button logout;
+    ImageView main_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logout=findViewById(R.id.button);
+        main_image=findViewById(R.id.main_image);
 
-        logout.setOnClickListener(new View.OnClickListener()
-        {
-
+        Thread clock_thread=new Thread(){
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this, Register.class));
-                finish();
+            public void run() {
+                super.run();
+
+                while(true)
+                {
+                    try {
+                        Thread.sleep(5000);
+                        if(main_image.getId()== android.content.res.)
+                        {
+                            main_image.setImageResource(R.drawable.berlino);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        });
+        };
     }
 }
