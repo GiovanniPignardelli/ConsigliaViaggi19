e0e3abc1e86474c7c10d9d73ab94103c66df9dfc
RalphTheCreator <raff.dimaso@studenti.unina.it>
2020-09-27 22:21:34 +0200

    patch: La MapExploreActivity è ora raggiungibile dalla ResultsActivity, che mostrerà sulle Maps di Google le strutture ricercate. patch: Implementata (MapExploreActivity.)ResultMarkerAdapter che ottiene i PlaceDAO per generare marker sulla mappa.

--- app/src/main/java/it/gpgames/consigliaviaggi19/search/ResultsActivity.java
+++ app/src/main/java/it/gpgames/consigliaviaggi19/search/ResultsActivity.java
@@ -7,15 +7,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import it.gpgames.consigliaviaggi19.R;
 import it.gpgames.consigliaviaggi19.network.NetworkChangeReceiver;
 import it.gpgames.consigliaviaggi19.places.Place;
 import it.gpgames.consigliaviaggi19.search.place_details.PlaceDetailsActivity;
+import it.gpgames.consigliaviaggi19.search.place_map.MapExploreActivity;
 
 /** Activity che si occupa di visualizzare i risultati di una query.
  * A questa activity viene anche passata la stringa di ricerca tramite intent*/
@@ -24,8 +30,10 @@ public class ResultsActivity extends AppCompatActivity {
     private static android.content.Context context;
     private TextView titleText;
     private ImageView bBack;
+    private Button bMapExplore;
     private RecyclerView resultQueries;
     private QueryResultsAdapter adapter;
+    private List<Place> lastQuery;
     private static final NetworkChangeReceiver networkChangeReceiver=NetworkChangeReceiver.getNetworkChangeReceiverInstance();
 
     /** Il metodo si occupa anche di generare il primo QueryExecutor.*/
@@ -34,14 +42,18 @@ public class ResultsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
         bBack = findViewById(R.id.back2);
+        bMapExplore = findViewById(R.id.mapExplore);
         titleText= findViewById(R.id.titleResultText);
         resultQueries = findViewById(R.id.resultQueries);
+
         initListeners();
+
         context=this.getApplicationContext();
+
         String querySearchString = getIntent().getStringExtra("searchString");
         titleText.setText("Ecco cosa abbiamo trovato per "+"\""+querySearchString+"\"");
+
         QueryExecutor executor=new QueryExecutor(parseString(querySearchString, " "),this);
-        Log.d("query", "Executor creato. La query sta per partire.");
         executor.executeQuery();
     }
 
@@ -72,16 +84,11 @@ public class ResultsActivity extends AppCompatActivity {
     /** Quando si ottengono i risultati di una query, si passano le liste ottenute a questo metodo che si occupa
      * di organizzare i risultati nella scrollView dell'activity. Per farlo si serve dell'adapter QueryResultAdapter.*/
     public void setUpRecycleView(List<Place> weakList, List<Place> topList) {
-        if(topList==null)
-            Log.d("query","Topquery=null");
-        else
-            {
-                for(Place place: topList)
-                    Log.d("query",place.getName());
-            }
         adapter = new QueryResultsAdapter(ResultsActivity.this, topList, this);
+        lastQuery = topList;
         resultQueries.setAdapter(adapter);
         resultQueries.setLayoutManager(new LinearLayoutManager(ResultsActivity.this, RecyclerView.VERTICAL, false));
+        bMapExplore.setEnabled(true);
     }
 
 /** Splitta una stringa con un dato pivot. Restituisce un array delle dimensioni del numero di parole della stringa.*/
@@ -94,6 +101,17 @@ public class ResultsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        bMapExplore.setEnabled(false);
+        bMapExplore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMapExplore = new Intent(ResultsActivity.this, MapExploreActivity.class);
+                Bundle queryBundle = new Bundle();
+                queryBundle.putSerializable("query", (Serializable) lastQuery);
+                toMapExplore.putExtra("query",queryBundle);
+                startActivity(toMapExplore);
             }
         });
     }
---
+++ app/src/main/java/it/gpgames/consigliaviaggi19/search/place_map/MapExploreActivity.java
@@ -0,0 +1,175 @@
+package it.gpgames.consigliaviaggi19.search.place_map;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.gpgames.consigliaviaggi19.R;
+import it.gpgames.consigliaviaggi19.home.MainActivity;
+import it.gpgames.consigliaviaggi19.places.Place;
+import it.gpgames.consigliaviaggi19.search.place_details.PlaceDetailsActivity;
+import it.gpgames.consigliaviaggi19.userpanel.UserData;
+
+public class MapExploreActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private class ResultMarkerAdapter
+    {
+        private List<Place> results = resultsToShow;
+
+        public void startAdapter(){
+            List<Marker> resultMarkers = new ArrayList<>();
+            for(Place p : results){
+                resultMarkers.add(createResultMarker(p));
+            }
+        }
+
+        public Marker createResultMarker(Place p){
+            LatLng currentLatLng = new LatLng(Double.parseDouble(p.getLatitude()),Double.parseDouble(p.getLongitude()));
+            MarkerOptions markerOptions = new MarkerOptions().position(currentLatLng).title(p.getName()).snippet("Media recens.: "+p.getAvgReview().toString());
+            switch(p.getCategory()){
+                case "hotel": markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.hotelmarker));
+                    break;
+                case "place": markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.placemarker));
+                    break;
+                case "restaurant": markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.foodmarker));
+                    break;
+            }
+            Marker placeMarker = mMap.addMarker(markerOptions);
+            placeMarker.setTag(p);
+            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker m) {
+                    Intent intent = new Intent(MapExploreActivity.this, PlaceDetailsActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra("toShow", (Place) m.getTag());
+                    MapExploreActivity.this.startActivity(intent);
+                    return true;
+                }
+            });
+            placeMarker.showInfoWindow();
+            return placeMarker;
+        }
+    }
+
+    private ActivityCompat callingActivity;
+    private FusedLocationProviderClient client;
+    private ImageView bBack;
+    private SupportMapFragment fMap;
+    private GoogleMap mMap;
+    private List<Place> resultsToShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_explore);
+        bBack = findViewById(R.id.back3);
+
+        resultsToShow = (List<Place>) getIntent().getBundleExtra("query").getSerializable("query");
+
+        FragmentManager fm = getSupportFragmentManager();
+        fMap = (SupportMapFragment) fm.findFragmentById(R.id.map);
+
+        initListeners();
+        checkLocationPermissions();
+    }
+
+    private void initListeners(){
+        bBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    /** Controlla la permission ACCESS_FINE_LOCATION.
+     * - PERMISSION_GRANTED: la mappa è inizializzata nel SupportMapFragment e punta alla currentLocation dell'user.
+     * - PERMISSION_DENIED: requestPermissions() è chiamato, riducendoci al caso precedente.
+     */
+    private void checkLocationPermissions(){
+        if(ActivityCompat.checkSelfPermission(MapExploreActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            client = LocationServices.getFusedLocationProviderClient(this);
+            fMap.getMapAsync(this);
+            Task<Location> task = client.getLastLocation();
+            initMapView(task);
+        }
+        else{
+           ActivityCompat.requestPermissions(MapExploreActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+
+    }
+
+    /** Questo metodo viene chiamato dopo il checkLocationPermissions() per inizializzare la mappa e il relativo currentLocation.*/
+    private void initMapView(Task task){
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null) {
+                    LatLng currentLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+                    MarkerOptions markerOptions = new MarkerOptions().position(currentLatLng).title("Sei qui!").icon(BitmapDescriptorFactory.fromResource(R.drawable.currentpos)).snippet(UserData.getLocalInstance().getDisplayName());
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,10));
+                    Marker userMarker = mMap.addMarker(markerOptions);
+                    setupResultMarkerAdapter();
+                }
+                else{
+                    finish();
+                }
+            }
+        });
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                checkLocationPermissions();
+            }
+            else{
+                Toast.makeText(this,"Autorizza la localizzazione prima di continuare.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    public void setupResultMarkerAdapter(){
+        ResultMarkerAdapter ra = new ResultMarkerAdapter();
+        ra.startAdapter();
+    }
+}
